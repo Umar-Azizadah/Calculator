cb7593da61326c717bff856d8b6a9e18bfe6489d
Umar-Azizadah <uoazizadah@cpp.edu>
2025-03-11 23:12:39 -0700

    Forgot to add each new operations under and overflow tests, fixed now

--- test_calculator.c
+++ test_calculator.c
@@ -30,6 +30,15 @@ void test_add_zero(void) {
     TEST_ASSERT_EQUAL(10,add(10, 0)); //expect 10 + 0 = 10
     TEST_ASSERT_EQUAL(0,add(0,0)); //expect 0 + 0 = 10
 }
+void test_add_overflow(void) {
+    int result = add(INT_MAX,1); //should wrap arround or cause undefined behavior
+    TEST_ASSERT_TRUE(result < 0); //this checks if overflow occured
+}
+
+void test_add_underflow(void) {
+    int result = add(INT_MIN,-1); 
+    TEST_ASSERT_TRUE(result > 0); //this checks if underflow occured
+}
 
 //subtract test cases 
 void test_subtract_positive_numbers(void){
@@ -45,6 +54,16 @@ void test_subtract_zero(void) {
     TEST_ASSERT_EQUAL(10,subtract(10, 0)); //expect 10 - 0 = 10
     TEST_ASSERT_EQUAL(0,subtract(0,0)); //expect 0 - 0 = 10
 }
+void test_subtract_overflow(void) {
+    int result = subtract(INT_MAX, -1); 
+    TEST_ASSERT_TRUE(result < 0);
+}
+
+void test_subtract_underflow(void) {
+    int result = subtract(INT_MIN, 1);  
+    TEST_ASSERT_TRUE(result > 0); 
+}
+
 
 //multiply test cases 
 void test_multiply_positive_numbers(void){
@@ -60,6 +79,16 @@ void test_multiply_zero(void) {
     TEST_ASSERT_EQUAL(0,multiply(10, 0)); //expect 10 * 0 = 0
     TEST_ASSERT_EQUAL(0,multiply(0,0)); //expect 0 * 0 = 0
 }
+void test_multiply_overflow(void) {
+    int result = multiply(INT_MAX, 2);
+    TEST_ASSERT_TRUE(result < 0); 
+}
+
+void test_multiply_underflow(void) {
+    int result = multiply(INT_MIN, -1); 
+    TEST_ASSERT_TRUE(result < 0); 
+}
+
 
 //divide test cases
 void test_divide_positive_numbers(void){
@@ -75,16 +104,11 @@ void test_divide_zero(void) {
     TEST_ASSERT_EQUAL(0,divide(10, 0)); //expect 10 / 0 = 0
     TEST_ASSERT_EQUAL(0,divide(0,0)); //expect 0 / 0 = 0
 }
-
-void test_add_overflow(void) {
-    int result = add(INT_MAX,1); //should wrap arround or cause undefined behavior
-    TEST_ASSERT_TRUE(result < 0); //this checks if overflow occured
+void test_divide_overflow(void) {
+    int result = divide(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result > 0); 
 }
 
-void test_add_underflow(void) {
-    int result = add(INT_MIN,-1); 
-    TEST_ASSERT_TRUE(result > 0); //this checks if underflow occured
-}
 
 int main (void) {
     UNITY_BEGIN();
@@ -93,27 +117,33 @@ int main (void) {
     RUN_TEST(test_add_positive_numbers_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
+    RUN_TEST(test_add_overflow);
+    RUN_TEST(test_add_underflow);
 
     //run subtract tests
     RUN_TEST(test_subtract_positive_numbers);
     RUN_TEST(test_subtract_positive_numbers_and_negative_numbers);
     RUN_TEST(test_subtract_negative_numbers);
     RUN_TEST(test_subtract_zero);
+    RUN_TEST(test_subtract_overflow);
+    RUN_TEST(test_subtract_underflow);
 
     //run multiply tests
     RUN_TEST(test_multiply_positive_numbers);
     RUN_TEST(test_multiply_positive_numbers_and_negative_numbers);
     RUN_TEST(test_multiply_negative_numbers);
     RUN_TEST(test_multiply_zero);
+    RUN_TEST(test_multiply_overflow);
+    RUN_TEST(test_multiply_underflow);
 
     //run divide tests
     RUN_TEST(test_divide_positive_numbers);
     RUN_TEST(test_divide_negative_numbers);
     RUN_TEST(test_divide_positive_numbers_and_negative_numbers);
     RUN_TEST(test_divide_zero);
+    RUN_TEST(test_divide_overflow);
 
-    RUN_TEST(test_add_overflow);
-    RUN_TEST(test_add_underflow);
+    
 
     return UNITY_END();
 }
