21bd863b17e3c55463e20d3d742fccb269534f30
Umar-Azizadah <uoazizadah@cpp.edu>
2025-03-11 17:06:15 -0700

    Added 3 more tests

--- test_calculator.c
+++ test_calculator.c
@@ -19,8 +19,25 @@ void test_add_positive_numbers(void){
     TEST_ASSERT_EQUAL(5, add(2,3)); //we expect 2+3=5
 }
 
+void test_add_positive_numbers_and_negative_numbers(void){
+    TEST_ASSERT_EQUAL(1, add(5,-4)); //expect 5 + -4 =1
+}
+
+void test_add_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-7,add(-3,-4)); //expect -3 + -4 = -7
+}
+
+//NOTE we can do multiple validations in one test
+void test_add_zero(void) {
+    TEST_ASSERT_EQUAL(10,add(10, 0)); //expect 10 + 0 = 10
+    TEST_ASSERT_EQUAL(0,add(0,0)); //expect 0 + 0 = 10
+}
+
 int main (void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);// run the rest function
+    RUN_TEST(test_add_positive_numbers_and_negative_numbers);
+    RUN_TEST(test_add_negative_numbers);
+    RUN_TEST(test_add_zero);
     return UNITY_END();
 }
